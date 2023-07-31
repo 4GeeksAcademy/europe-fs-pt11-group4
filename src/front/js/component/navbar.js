d9214147b43b1bc21763b6a34baaaa93e5a35e1f
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-07-31 09:47:02 +0000

    successfully imported logo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import myDoctorUrl from "../../img/mydoctor.png";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img src={myDoctorUrl} alt="my doctor logo" style={{ width: '70px', }} />
+					<span className="navbar-brand mb-0 h1">myDoctor</span>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
