07cdf805785362d94b1873049dbff03091981443
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-07-31 12:02:10 +0000

    changed font color to match logo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import myDoctorUrl from "../../img/mydoctor.png";
+import "../../styles/home.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+		<nav className="navbar">
+			<div className="container-fluid">
 				<Link to="/">
 					<img src={myDoctorUrl} alt="my doctor logo" style={{ width: '70px', }} />
-					<span className="navbar-brand mb-0 h1">myDoctor</span>
+					<span className="brand mb-0 h1">myDoctor</span>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in</button>
+						<button className="btn btn-primary">Sign In</button>
 					</Link>
 				</div>
 			</div>
