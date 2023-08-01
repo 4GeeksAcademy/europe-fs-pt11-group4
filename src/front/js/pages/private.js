61d13645f9b1602c945ba33e604d631fca437ead
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-08-01 16:09:42 +0000

    add react tabs and initial setup

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,26 +1,49 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import Nav from 'react-bootstrap/Nav';
-import doctor3ImageUrl from "../../img/doctor3.jpg";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import "../../styles/home.css";
 
 export const Private = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<Nav justify variant="tabs" defaultActiveKey="/home">
-			<Nav.Item>
-				<Nav.Link eventKey="link-1">Active</Nav.Link>
-			</Nav.Item>
-			<Nav.Item>
-				<Nav.Link eventKey="link-2">Loooonger NavLink</Nav.Link>
-			</Nav.Item>
-			<Nav.Item>
-				<Nav.Link eventKey="link-3">Link</Nav.Link>
-			</Nav.Item>
-			<Nav.Item>
-				<Nav.Link eventKey="link-4">Link</Nav.Link>
-			</Nav.Item>
-		</Nav>
+		<div className="container">
+			<Tabs>
+				<TabList>
+					<Tab>
+						User home <i class="fa-solid fa-house-chimney-user"></i>
+					</Tab>
+					<Tab>
+						Medical history and reports <i class="fa-solid fa-notes-medical"></i>
+					</Tab>
+					<Tab>
+						Book video consultation <i class="fa-solid fa-laptop-medical"></i>
+					</Tab>
+					<Tab>
+						Invoices <i class="fa-solid fa-file-invoice"></i>
+					</Tab>
+				</TabList>
+
+				<TabPanel>
+					<h2>Welcome back John! </h2>
+					<h3>Upcoming appointments</h3>
+
+				</TabPanel>
+				<TabPanel>
+					<h2>Any content 2</h2>
+				</TabPanel>
+				<TabPanel>
+					<h3>Book your video appointment in three easy steps</h3>
+					<h4>Select doctor specialty <i class="fa-solid fa-user-doctor"></i></h4>
+					<h4>Select date and time <i class="fa-regular fa-calendar-days"></i></h4>
+					<h4>Add notes or comments</h4>
+				</TabPanel>
+				<TabPanel>
+					<h2>Any content 4</h2>
+				</TabPanel>
+			</Tabs>
+		</div>
 	);
 };
