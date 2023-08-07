3de5cce611c402ce49c4d6bf8e42a940430026dc
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-08-07 16:48:09 +0000

    login page adjusted to center of page

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -51,55 +51,57 @@ export const Login = () => {
 
 	return (
 
-		<div className="w-50 p-3">
-        <form>                               
-            <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: "1px" }}>Sign into your account</h5>
-                <div className="form-outline mb-4">
-                    <input type="email" id="form2Example17" className="form-control form-control-lg" name="email" value={email} onChange={handleInputChange} style={error.email ? errorStyle : {}} />
-                    <label className="form-label" >Email address{error.email && <label className="text-danger text-opacity-50 fst-italic lh-1">Email is required</label>}</label>
-                </div>
+		<div className="container ">
+            <div className="w-50 p-3 mx-auto border border-primary-subtle p-5">
+                <form>                               
+                    <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: "1px" }}>Sign into your account</h5>
+                        <div className="form-outline mb-4">
+                            <input type="email" id="form2Example17" className="form-control form-control-lg" name="email" value={email} onChange={handleInputChange} style={error.email ? errorStyle : {}} />
+                            <label className="form-label" >Email address{error.email && <label className="text-danger text-opacity-50 fst-italic lh-1">Email is required</label>}</label>
+                        </div>
 
-                <div className="form-outline mb-4">
-                    <input type="password" id="form2Example27" className="form-control form-control-lg" name="password" value={password} onChange={handleInputChange} style={error.password ? errorStyle : {}} />
-                    <label className="form-label" >Password {error.password && <label className="text-danger text-opacity-50 fst-italic lh-1">Password is required</label>}</label>
-                </div>
+                        <div className="form-outline mb-4">
+                            <input type="password" id="form2Example27" className="form-control form-control-lg" name="password" value={password} onChange={handleInputChange} style={error.password ? errorStyle : {}} />
+                            <label className="form-label" >Password {error.password && <label className="text-danger text-opacity-50 fst-italic lh-1">Password is required</label>}</label>
+                        </div>
 
-                <div className="pt-1 mb-4">
-                    <div className="form-check">
-                        <Link to="/forgot" >
-                            <p>Forgot Password?</p>
-                        </Link>
-                    </div>
-                    <button className="btn btn-primary " type="button" onClick={loginUserRequest}>Login</button>
+                        <div className="pt-1 mb-4">
+                            <div className="">
+                                <Link to="/forgot" >
+                                    <p>Forgot Password?</p>
+                                </Link>
+                            </div>
+                            <button className="btn btn-primary " type="button" onClick={loginUserRequest}>Login</button>
 
-                </div>
+                        </div>
 
-                
-                
-                <div className="mb-3 form-check">
-                    <Link to="/signup" >
-                        <h5>CREATE AN ACCOUNT</h5>
-                    </Link>
-                </div>
+                        
+                        
+                        <div className="mb-3 ">
+                            <Link to="/signup" >
+                                <h5>CREATE AN ACCOUNT</h5>
+                            </Link>
+                        </div>
 
-        </form>
+                </form>
 
 
 
-            <Modal show={showModal} onHide={() => setShowModal(false)}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Error</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    The login credentials are incorrect. Please verify your email and password.
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={() => setShowModal(false)}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+                    <Modal show={showModal} onHide={() => setShowModal(false)}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Error</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            The login credentials are incorrect. Please verify your email and password.
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={() => setShowModal(false)}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
 
+            </div>
         </div>
 
     );
