6c36b6fa95c1035b9a94bb84aed75fbb07cb1e07
sanghmitra2023 <128592793+sanghmitra2023@users.noreply.github.com>
2023-08-10 16:59:26 +0000

    change images

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,9 +6,9 @@ import CardTwo from "../component/cardTwo.jsx";
 import CardOne from "../component/card.jsx";
 
 
-const dataTwo = [{titleTwo:"What makes myDoctor a One-Stop Healthcare App", descriptionTwo:"One place to get initial consultation, diagnosis, second opinion, treatment plans, e-prescriptions, follow up, medical reports in e-copy and much more", detailOne:"Top doctors across Europe are onboarded from various disciplines including General Physicians, Physiotherapists, Psychiatrists, Dentists, ENT Specialists, Gynecologists, Cardiologists, Dermatologists, Neurologists, Pediatricians, and many others."}, 
-{titleTwo:"With so many such apps around Why Choose Us", descriptionTwo:"We are committed to providing effiecient, reliable and affordable digital healthcare services.", detailOne: "We combine world-class technology and top-rated doctors", detailTwo:"We bring experts from all fields of medicine at one place", detailThree: "On our team are doctors from all over Europe. No more language problems, no more geographical limitations"},
-{titleTwo:"What do We Offer to Our Clients", descriptionTwo:"High quality video calls", detailOne:"Assurance that all your data is safe with us", detailTwo:"Medical reports in soft copy", detailThree: "e-prescription to get your medicines from any pharmacy", detailFour: "Free second opinion from other doctors onboard"},
+const dataTwo = [{titleTwo:"What makes myDoctor a One-Stop Healthcare App", imageUrlTwo: "https://www.myhealthspace.in/assets/img/consult-min.jpg",descriptionTwo:"One place to get initial consultation, diagnosis, second opinion, treatment plans, e-prescriptions, follow up, medical reports in e-copy and much more", detailOne:"Top doctors across Europe are onboarded from various disciplines including General Physicians, Physiotherapists, Psychiatrists, Dentists, ENT Specialists, Gynecologists, Cardiologists, Dermatologists, Neurologists, Pediatricians, and many others."}, 
+{titleTwo:"With so many such apps around Why Choose Us", imageUrlTwo: "https://www.riomed.com/wp-content/uploads/2021/11/blogpost.jpg",descriptionTwo:"We are committed to providing effiecient, reliable and affordable digital healthcare services.", detailOne: "We combine world-class technology and top-rated doctors", detailTwo:"We bring experts from all fields of medicine at one place", detailThree: "On our team are doctors from all over Europe. No more language problems, no more geographical limitations"},
+{titleTwo:"What do We Offer to Our Clients", imageUrlTwo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKP9wGQrpXu5Ge_KbguhCsoqusqGJMZ4T7wA&usqp=CAU", descriptionTwo:"High quality video calls", detailOne:"Assurance that all your data is safe with us", detailTwo:"Medical reports in soft copy", detailThree: "e-prescription to get your medicines from any pharmacy", detailFour: "Free second opinion from other doctors onboard"},
  ,]
 
 const data = [{title:"Select a Speciality", description:"From our team of experienced doctors select the specialization or if you are not sure, you can book an appointment with a GP"}, 
@@ -38,7 +38,7 @@ export const Home = () => {
 				<div className="row gap-4 m-5">
 					{dataTwo.map(cardTwo =><CardTwo
 						titleTwo={cardTwo.titleTwo}
-						imageUrlTwo={"https://fakeimg.pl/500x325"}
+						imageUrlTwo={cardTwo.imageUrlTwo}
 						descriptionTwo={cardTwo.descriptionTwo}
 						detailOne={cardTwo.detailOne}
 						detailTwo={cardTwo.detailTwo}
